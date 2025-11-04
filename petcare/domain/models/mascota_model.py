1f2acd16e133874f6992ccecdab28175f4610cc3
EriGorosito <engorosito@estudiantes.unsam.edu.ar>
2025-11-04 20:33:22 +0000

    arreglar atributos

--- petcare/domain/models/mascota_model.py
+++ petcare/domain/models/mascota_model.py
@@ -1,14 +1,15 @@
 # petcare/domain/mascota.py
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, Enum as SQLEnum
 from sqlalchemy.orm import relationship
 from petcare.core.database import Base
+from petcare.domain.especie import Especie
 
 class Mascota(Base):
     __tablename__ = "mascotas"
 
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String, nullable=False)
-    especie = Column(String, nullable=False)
+    especie = Column(SQLEnum(Especie, name="especie_enum"), nullable=False)
     raza = Column(String)
     edad = Column(Integer)
     peso = Column(Float)
