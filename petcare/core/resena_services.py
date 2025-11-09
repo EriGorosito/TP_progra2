80ea4ea0f9ee65bf5e8f39345f0f7fbb6e3b1786
euge <eacikugel@estudiantes.unsam.edu.ar>
2025-11-08 22:35:49 -0300

    arreglar reseña

--- petcare/core/resena_services.py
+++ petcare/core/resena_services.py
@@ -1,4 +1,5 @@
-from petcare.domain.resena import Resena
+from petcare.domain.models.resena_model import Resena
+from petcare.tasks.update_reserva import actualizar_reservas_finalizadas
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status
 
@@ -8,17 +9,18 @@ from petcare.schemas.resena_schemas import ReviewCreate
 
 def create_review(db: Session, data: ReviewCreate):
 
+    actualizar_reservas_finalizadas()
     reserva = db.query(Reserva).filter(Reserva.id == data.reserva_id).first()
     if not reserva:
         raise HTTPException(status_code=404, detail="Reserva no encontrada")
 
-    if reserva.estado != "Finalizada":
+    if reserva.estado != "finalizada":
         raise HTTPException(
             status_code=400,
             detail="Solo se pueden dejar reseñas de reservas finalizadas"
         )
 
-    if reserva.review:
+    if reserva.resena:
         raise HTTPException(
             status_code=400,
             detail="La reserva ya tiene reseña"
@@ -47,4 +49,4 @@ def get_cuidador_puntaje(db: Session, cuidador_id: int):
     resenas = get_reviews_by_cuidador(db, cuidador_id)
     if not resenas:
         return 0
-    return sum(r.rating for r in resenas) / len(resenas)
+    return sum(r.puntaje for r in resenas) / len(resenas)
