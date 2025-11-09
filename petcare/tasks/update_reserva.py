d87bb44cd8351b1ceb0b93bd25d82aa0da0bd33e
euge <eacikugel@estudiantes.unsam.edu.ar>
2025-11-08 22:36:36 -0300

    actualizar antes de llamar a reseña

--- petcare/tasks/update_reserva.py
+++ petcare/tasks/update_reserva.py
@@ -1,21 +1,21 @@
-from datetime import datetime
+from datetime import date
 from sqlalchemy.orm import Session
 from petcare.core.database import SessionLocal
 from petcare.domain.models.reserva_model import Reserva
 
 def actualizar_reservas_finalizadas():
     db: Session = SessionLocal()
-    ahora = datetime.now()
+    ahora = date.today()
 
     try:
         reservas = (
             db.query(Reserva)
-            .filter(Reserva.estado == "confirmada", Reserva.fecha_fin < ahora)
+            .filter(Reserva.estado == "aceptada", Reserva.fecha_fin < ahora)
             .all()
         )
 
         for r in reservas:
-            r.estado = "Finalizada"
+            r.estado = "finalizada"
 
         if reservas:
             db.commit()
